31bee8063ca847e854894aa0dd48341fe0a6ddc3
ALIVKA <alivkakanada@gmail.com>
2024-07-26 13:57:46 +0300

    game

--- app/game/game.py
+++ app/game/game.py
@@ -1,5 +1,5 @@
-from aiogram import F, Router
-from aiogram.types import Message
+from aiogram import F, Router, types
+from aiogram.types import Message, CallbackQuery
 from app.__init__ import bot
 from app.handlers.game_handlers import choise
 import logging
@@ -13,15 +13,28 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-async def game (users_in_game):
+async def game(users_in_game):
     user_one, user_two = users_in_game
-    if random.randrange(1,3) == 1:
-        await bot.send_message(user_one[0], "❕Ход достается вам, выберите действие", reply_markup=choise)
-        await bot.send_message(user_two[0], "❗️Ход достается противнику, ожидайте его действие. ")
-    else:
-        await bot.send_message(user_two[0], "❕Ход достается вам, выберите действие", reply_markup=choise)
-        await bot.send_message(user_one[0], "❗️Ход достается противнику, ожидайте его действие. ")
+    first_player, second_player = random.sample([user_one, user_two], 2)
+    await bot.send_message(first_player[0], "❕Ход достается вам, выберите действие", reply_markup=choise)
+    await bot.send_message(second_player[0], "❗️Ход достается противнику, ожидайте его действие. ")
 
+    @router.callback_query(F.data == 'Стрелять в себя')
+    async def shoot(callback: types.CallbackQuery):
+        if callback.from_user.id == first_player[0]:
+            if random.randrange(1, 7) == 1:
+                await bot.send_message(first_player[0], "💀Приставив ствол револьвера к своему виску и спустив курок, всë кругом резко почернело, вы мертвы. ")
+                await bot.send_message(second_player[0], "😬Вы видите как противник спустил курок револьвера держа револьвер у своего виска, после чего остаётся с дыркой в голове. Противник мёртв. ")
+            else:
+                await bot.send_message(first_player[0], "😀Приставив ствол револьвера к своему виску и спустив курок, вы остались живы, ход остаётся у вас.", reply_markup=choise)
+                await bot.send_message(second_player[0], "Вы видите как противник спустил кунок револьвера держа револьвер у своего виска, противник остаётся живым, ход по прежнему у него. ")
+        elif callback.from_user.id == second_player[0]:
+            if random.randrange(1, 7) == 1:
+                await bot.send_message(second_player[0], "💀Приставив ствол револьвера к своему виску и спустив курок, всë кругом резко почернело, вы мертвы. ")
+                await bot.send_message(first_player[0], "😬Вы видите как противник спустил кунок револьвера держа револьвер у своего виска, после чего остаётся с дыркой в голове. Противник мёртв. ")
+            else:
+                await bot.send_message(second_player[0], "😀Приставив ствол револьвера к своему виску и спустив курок, вы остались живы, ход остаётся у вас.", reply_markup=choise)
+                await bot
 
     
         
